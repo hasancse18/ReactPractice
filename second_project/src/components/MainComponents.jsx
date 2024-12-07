23cc96d9c0718929c436205eb924ac4bdd1a3e8e
hasancse18 <hasan7779296@gmail.com>
2024-12-07 21:36:52 +0600

    Working in Second Project adding backgorund Image and styling nav

---
+++ second_project/src/components/MainComponents.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MainComponents = () => {
+  return (
+        <main>
+            <h1>Fun Facts</h1>
+            <ul className='facts-list'>
+                <li>Was First released in 2013</li>
+                <li>Was orginally created by Jordan Walke</li>
+                <li>Has well over 200k stars on Github</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+  )
+}
+
+export default MainComponents
---
+++ second_project/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+        <header>
+            <nav>
+                <img src="./src/assets/react.svg" alt="not get" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+  )
+}
+
+export default Navbar
