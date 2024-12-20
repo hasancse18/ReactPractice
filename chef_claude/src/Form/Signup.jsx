b0c6e44c86096a83962ae58c59ade9450cd5b44e
hasancse18 <hasan7779296@gmail.com>
2024-12-20 20:10:07 +0600

    Working On Form Data

--- chef_claude/src/Form/Signup.jsx
+++ chef_claude/src/Form/Signup.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import "./style.css"
-const Signup = () => {
+import React, { useState } from 'react';
+import "./style.css";
 
-    
+const Signup = () => {
+  const initialValues = { email: "", password: "" };
+  const [value, setValue] = useState(initialValues);
+  //console.log(value)
+  // const handleChange = (event) => {
+  //   const { name, value } = event.target;
+  //   setValue((prevValue) => ({
+  //     ...prevValue,
+  //     [name]: value,
+  //   }));
+  // };
+
   function signUp(event) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget)
@@ -10,62 +20,81 @@ const Signup = () => {
     const password = formData.get("password")
     const status = formData.get("status")
     const diet = formData.getAll("diet")
-    console.log(diet)
-  }
+    const favColor = formData.get("favColor")
+    //console.log(favColor);
+    
+   setValue({email,password});
 
+  }
+  
   return (
     <section>
       <h1>Signup form</h1>
       <form onSubmit={signUp}>
-
         <label htmlFor="email">Email:</label>
-        <input id="email" defaultValue="joe@schmoe.com" type="email" name="email" placeholder="joe@schmoe.com" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="joe@schmoe.com"
+          //value={value.email}
+          //onChange={handleChange}
+        />
 
         <label htmlFor="password">Password:</label>
-        <input id="password" defaultValue="password123" type="password" name="password" />
+        <input
+          id="password"
+          type="password"
+          name="password"
+          //value={value.password}
+          //onChange={handleChange}
+        />
+        
         <label htmlFor="description">Description</label>
         <textarea name="description" id="description" defaultValue="This is description"></textarea>
         
         <fieldset>
           <legend>Employment Status</legend>
-          <label >
-            <input type="radio" name = "status" value="Unemployed"/>
+          <label>
+            <input type="radio" name="status" value="Unemployed" />
             Unemployed
           </label>
-          <label >
-            <input type="radio" name = "status" value="Part-Time"/>
+          <label>
+            <input type="radio" name="status" value="Part-Time" />
             Part-Time
           </label>
-          <label >
-            <input type="radio" name = "status" defaultChecked={true} value="Full-Time"/>
+          <label>
+            <input type="radio" name="status" defaultChecked={true} value="Full-Time" />
             Full-Time
           </label>
-
         </fieldset>
+        
         <fieldset>
-          <legend>Dietry restrictions</legend>
-          <label >
-            <input type="checkbox" name = "diet" value="kosher"/>
+          <legend>Dietary Restrictions</legend>
+          <label>
+            <input type="checkbox" name="diet" value="Kosher" />
             Kosher
           </label>
-          <label >
-            <input type="checkbox" name = "diet" value="Vegan"/>
+          <label>
+            <input type="checkbox" name="diet" value="Vegan" />
             Vegan
           </label>
-          <label >
-            <input type="checkbox" name = "diet" value="Gluten-free"/>
+          <label>
+            <input type="checkbox" name="diet" value="Gluten-free" />
             Gluten-free
           </label>
-
         </fieldset>
-        
-        
-
-        
-        <button>Submit</button>
-
+        <label htmlFor="favColor">What Color You Like Most?</label>
+        <select name="favColor" id="favColor">
+          <option value="Green">Green</option>
+          <option value="Violet">Violet</option>
+          <option value="Red">Red</option>
+          <option value="Gray">Gray</option>
+        </select>
+        <button type="submit">Submit</button>
       </form>
     </section>
-  )
-}
-export default Signup
+  );
+};
+
+export default Signup;
